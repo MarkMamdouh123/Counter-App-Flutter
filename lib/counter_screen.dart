efb9621c31ed984ede41910b79e3c535f4c86bf7
Mark Mamdouh <mark.mamdouh67@yahoo.com>
2024-01-31 00:37:50 +0200

    made counter app

---
+++ lib/counter_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+int counter = 1;
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Center(
+            child: Text(
+              'Counter',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w900),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    counter++;
+                  });
+                },
+                child: const Text(
+                  'Plus',
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  '$counter',
+                  style: const TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    if (counter == 0) {
+                      return;
+                    }
+                    counter--;
+                  });
+                },
+                child: const Text(
+                  'Minus',
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
